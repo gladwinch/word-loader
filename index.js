2f4b067b31cb256cfc25396033a05706fc17dbda
gladwinch <gladwinch20@gmail.com>
2022-12-02 17:11:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ success: true, data: "gladwin" })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is connected on port ${PORT}`)
+})
