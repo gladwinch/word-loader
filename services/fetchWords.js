099f3d892b308f7e6692ab0140f440582248f845
gladwinch <gladwinch20@gmail.com>
2022-12-03 14:21:20 +0530

    select index algorithm update

--- services/fetchWords.js
+++ services/fetchWords.js
@@ -4,16 +4,37 @@ const db = require('../db')
 
 async function fetchWords () {
     // get list id
+    let lists = await db.fetchList()
+    let listKey = selectIndex(lists)
 
-
-    // new-word -> 4
-    // learn-word -> 3
-    // practice-word -> 2
-    // context-practice -> 2
-    // known-word -> 1
-
+    if(!listKey) {
+        // :TODO resest list db
+        return
+    }
+    
+    
 }
 
-fetchWords()
+// helper fn
+
+function selectIndex(list) {
+    let listArr = Object.keys(list)
+        .map((key) => [key, list[key]])
+        .filter(list => list[1] > 0)
+
+    let selectedIndex = getRandomInt(0, listArr.length - 1)
+    if(!listArr.length) null
+
+    return listArr[selectedIndex][0]
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// fetchWords()
+selectIndex(list)
 
 // module.exports = fetchWOrds
